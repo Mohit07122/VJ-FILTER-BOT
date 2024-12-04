2ee37afc54299f077fb44c9c5439a249e9a2bcc5
Mohit07122 <125998048+Mohit07122@users.noreply.github.com>
2024-12-04 19:00:37 +0530

    Update info.py

--- info.py
+++ info.py
@@ -138,7 +138,7 @@ if 'DYNO' in environ:
     ON_HEROKU = True
 else:
     ON_HEROKU = False
-URL = environ.get("URL", "https://newcollegapp1-fs32u61c.b4a.run/")
+URL = environ.get("URL", "https://autofilter-40fam4xu.b4a.run/")
 
 
 # Rename Info : If True Then Bot Rename File Else Not
